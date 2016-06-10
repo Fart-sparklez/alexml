43db51e46baea49e4a5c445e1522b94217ab981d
gforcedev <tecshotclark@gmail.com>
2016-06-10 15:52:05 +0100

    Fixed crash and changed background color

--- core/src/me/tgr/test/testGame.java
+++ core/src/me/tgr/test/testGame.java
@@ -112,7 +112,7 @@ public class testGame extends ApplicationAdapter implements EventListener {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		stage.act();
