47471a1d929772c512359e14ec30b8f5c6385d85
gforcedev <tecshotclark@gmail.com>
2016-06-11 19:50:23 +0100

    Added Desktoplauncher file to git ????

---
+++ desktop/src/me/gforcedev/alexeditor/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package me.gforcedev.alexeditor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.gforcedev.alexeditor.AlexEditorWindow;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+		new LwjglApplication(new AlexEditorWindow(), config);
+	}
+}
